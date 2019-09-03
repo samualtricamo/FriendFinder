b6c3143492e5672dfd791c16cdfab3d5f9771a5c
Sam Tricamo <samualtricamo@gmail.com>
2019-09-03 15:43:08 -0500

    added to server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
