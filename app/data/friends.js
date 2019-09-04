aeaa4691224adfc9ac328f9c7fcf32d3accd1cf6
Sam Tricamo <samualtricamo@gmail.com>
2019-09-04 07:36:35 -0500

    friends added

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friendsData = [
+    {
+        name: "Sam Tricamo",
+        photo: https://vignette.wikia.nocookie.net/parody/images/e/e6/Tumblr_niy7qsyO0T1r573nqo1_400.png/revision/latest?cb=20181116170419,
+        scores: []
+    },
+    {
+        name: "TJ",
+        photo: https://vignette.wikia.nocookie.net/recess/images/0/03/Tj-detweiler.png/revision/latest?cb=20150710095836,
+        scores: []
+    },
+    {
+        name: "Spinelli",
+        photo: https://i.pinimg.com/236x/4b/e9/5c/4be95c3493cd76c293777eb870b09fbf--halloween--halloween-ideas.jpg,
+        scores: []
+    },
+    {
+        name: "Gus",
+        photo: https://lh3.googleusercontent.com/tsUg1mtfl63BuhrXaXDyQs-45woyNb6hwpVgy7anvRf1GueagTieFapaqvEuDWQVETvKppM=s85,
+        scores: []
+    },
+    {
+        name: "Vince",
+        photo: https://vignette.wikia.nocookie.net/recess/images/0/0d/Vince_%283%29.jpg/revision/latest?cb=20150527113641,
+        scores: []
+    },
+    {
+        name: "Mikey",
+        photo: https://vignette.wikia.nocookie.net/legendsofthemultiuniverse/images/6/66/Recess-_School%27s_Out_Movie.mp4_001017083.png/revision/latest/scale-to-width-down/380?cb=20160726220306,
+        scores: []
+    },
+    {
+        name: "Gretchen",
+        photo: https://cdn.costumewall.com/wp-content/uploads/2015/09/gretchen-grundler-costume.jpg,
+        scores: []
+    },
+]
