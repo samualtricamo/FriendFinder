fd83df77df92b10313f6bec22ba0c6351105fdaa
Sam Tricamo <samualtricamo@gmail.com>
2019-09-05 16:08:27 -0500

    added character scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,38 +1,38 @@
 var friendsData = [
     {
         name: "Sam Tricamo",
-        photo: https://vignette.wikia.nocookie.net/parody/images/e/e6/Tumblr_niy7qsyO0T1r573nqo1_400.png/revision/latest?cb=20181116170419,
-        scores: []
+        photo: 'https://vignette.wikia.nocookie.net/parody/images/e/e6/Tumblr_niy7qsyO0T1r573nqo1_400.png/revision/latest?cb=20181116170419',
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     },
     {
         name: "TJ",
-        photo: https://vignette.wikia.nocookie.net/recess/images/0/03/Tj-detweiler.png/revision/latest?cb=20150710095836,
-        scores: []
+        photo: 'https://vignette.wikia.nocookie.net/recess/images/0/03/Tj-detweiler.png/revision/latest?cb=20150710095836',
+        scores: [5, 1, 2, 3, 4, 1, 2, 3, 3, 4]
     },
     {
         name: "Spinelli",
-        photo: https://i.pinimg.com/236x/4b/e9/5c/4be95c3493cd76c293777eb870b09fbf--halloween--halloween-ideas.jpg,
-        scores: []
+        photo: 'https://i.pinimg.com/236x/4b/e9/5c/4be95c3493cd76c293777eb870b09fbf--halloween--halloween-ideas.jpg',
+        scores: [1, 2, 3, 4, 1, 3, 4, 4, 5, 3]
     },
     {
         name: "Gus",
-        photo: https://lh3.googleusercontent.com/tsUg1mtfl63BuhrXaXDyQs-45woyNb6hwpVgy7anvRf1GueagTieFapaqvEuDWQVETvKppM=s85,
-        scores: []
+        photo: 'https://lh3.googleusercontent.com/tsUg1mtfl63BuhrXaXDyQs-45woyNb6hwpVgy7anvRf1GueagTieFapaqvEuDWQVETvKppM=s85',
+        scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
     },
     {
         name: "Vince",
-        photo: https://vignette.wikia.nocookie.net/recess/images/0/0d/Vince_%283%29.jpg/revision/latest?cb=20150527113641,
-        scores: []
+        photo: 'https://vignette.wikia.nocookie.net/recess/images/0/0d/Vince_%283%29.jpg/revision/latest?cb=20150527113641',
+        scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
     },
     {
         name: "Mikey",
-        photo: https://vignette.wikia.nocookie.net/legendsofthemultiuniverse/images/6/66/Recess-_School%27s_Out_Movie.mp4_001017083.png/revision/latest/scale-to-width-down/380?cb=20160726220306,
-        scores: []
+        photo: 'https://vignette.wikia.nocookie.net/legendsofthemultiuniverse/images/6/66/Recess-_School%27s_Out_Movie.mp4_001017083.png/revision/latest/scale-to-width-down/380?cb=20160726220306',
+        scores: [5, 4, 3, 2, 1, 4, 5, 2, 1, 5]
     },
     {
         name: "Gretchen",
-        photo: https://cdn.costumewall.com/wp-content/uploads/2015/09/gretchen-grundler-costume.jpg,
-        scores: []
+        photo: 'https://cdn.costumewall.com/wp-content/uploads/2015/09/gretchen-grundler-costume.jpg',
+        scores: [4, 3, 3, 3, 3, 5, 4, 1, 1, 1]
     },
 ]
 
