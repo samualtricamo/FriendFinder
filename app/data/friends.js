9a7383b4030c47560c2e008435de94866394e860
Sam Tricamo <samualtricamo@gmail.com>
2019-09-05 21:06:14 -0500

    heroku connected, characters specified, js created

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,32 +7,32 @@ var friendMatch = [
     {
         name: "TJ",
         photo: 'https://vignette.wikia.nocookie.net/recess/images/0/03/Tj-detweiler.png/revision/latest?cb=20150710095836',
-        scores: [5, 1, 2, 3, 4, 1, 2, 3, 3, 4]
+        scores: [5, 3, 3, 2, 2, 2, 5, 3, 1, 1]
     },
     {
         name: "Spinelli",
         photo: 'https://i.pinimg.com/236x/4b/e9/5c/4be95c3493cd76c293777eb870b09fbf--halloween--halloween-ideas.jpg',
-        scores: [1, 2, 3, 4, 1, 3, 4, 4, 5, 3]
+        scores: [1, 3, 4, 5, 2, 3, 5, 5, 1, 3]
     },
     {
         name: "Gus",
         photo: 'https://lh3.googleusercontent.com/tsUg1mtfl63BuhrXaXDyQs-45woyNb6hwpVgy7anvRf1GueagTieFapaqvEuDWQVETvKppM=s85',
-        scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+        scores: [2, 4, 2, 2, 4, 3, 5, 2, 3, 3]
     },
     {
         name: "Vince",
         photo: 'https://vignette.wikia.nocookie.net/recess/images/0/0d/Vince_%283%29.jpg/revision/latest?cb=20150527113641',
-        scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+        scores: [4, 5, 5, 3, 3, 2, 5, 3, 2, 2]
     },
     {
         name: "Mikey",
         photo: 'https://vignette.wikia.nocookie.net/legendsofthemultiuniverse/images/6/66/Recess-_School%27s_Out_Movie.mp4_001017083.png/revision/latest/scale-to-width-down/380?cb=20160726220306',
-        scores: [5, 4, 3, 2, 1, 4, 5, 2, 1, 5]
+        scores: [3, 2, 1, 2, 4, 5, 5, 1, 3, 2]
     },
     {
         name: "Gretchen",
         photo: 'https://cdn.costumewall.com/wp-content/uploads/2015/09/gretchen-grundler-costume.jpg',
-        scores: [4, 3, 3, 3, 3, 5, 4, 1, 1, 1]
+        scores: [1, 1, 1, 2, 5, 5, 5, 5, 3, 2]
     },
 ]
 
